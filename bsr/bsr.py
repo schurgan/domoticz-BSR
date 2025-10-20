d0855929967730a3fd7bccf466bfb351183de655
schurgan <51407784+schurgan@users.noreply.github.com>
2025-10-20 18:18:55 +0200

    Update bsr.py

--- bsr/bsr.py
+++ bsr/bsr.py
@@ -761,30 +761,21 @@ def calculateAlarmLevel(wasteDate):
 
 
 def scanAndParse(entry, wasteData: WasteData):
-    image = None
+    image = None  # JSON enthält aktuell keine Bilder
     now = datetime.now().date()
-    try:
-        image = tag.find("img")
-    except Exception as e:
-        pass
-    if (
-        wasteData.isEmpty()
-        and entry['category'] == wasteData.category 
-        ): 
+
+    if wasteData.isEmpty() and entry.get('category') == wasteData.category:
         Domoticz.Debug("found matching entry for {}".format(wasteData.wasteType))
         try:
-            
-            if entry['serviceDate_actual'] is not None :
-                service_date = datetime.strptime( entry['serviceDate_actual'], "%d.%m.%Y").date()
+            service_date_str = entry.get('serviceDate_actual')
+            if service_date_str is not None:
+                service_date = datetime.strptime(service_date_str, "%d.%m.%Y").date()
                 wasteData.wasteDate = service_date
-                wasteData.wasteType = entry['category']
-                wasteData.wasteHint = entry['warningText']
-                if image is not None:
-                    wasteData.wasteImage = image["src"]
-                    Domoticz.Debug("img: {}".format(image))
+                wasteData.wasteType = entry.get('category')
+                wasteData.wasteHint = entry.get('warningText', '')
+                wasteData.wasteImage = None  # aktuell kein Bild vorhanden
             else:
-                Domoticz.Debug("Skip entry,no date... {}".format(entry['serviceDate_actual']))
-            
+                Domoticz.Debug("Skip entry, no date... {}".format(entry))
         except Exception as e:
             Domoticz.Error(
                 "Could not parse content -> data {}\tentry {} ... exc: {} ".format(
